90d21800ccbcfa6f6a01b4d38f7d8174ee7de689
Webster <472680811@qq.com>
2025-07-14 14:12:30 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -201,7 +201,7 @@ const API_CONFIG = {
     },
     detail: {
         // 只拼接参数部分
-        path: '?ac=list&ids=',
+        path: '?ac=detail&ids=',
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
             'Accept': 'application/json'

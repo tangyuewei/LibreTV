a2d3d82d9a2b4d8d2f866e5de344274fb60ae622
Webster <472680811@qq.com>
2025-07-23 14:12:41 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 默认选中资源
-        selectedAPIs = ["tyyszy", "bfzy", "dyttzy", "ruyi"];
+        selectedAPIs = ["tyyszy","dyttzy","bfzy","ruyi","qilin","niuniu","fengcao","suoni","ffzy","heimuer","zy360","maotai","dbzy","mdzy","jisu","wujin"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
 
         // 默认选中过滤开关
